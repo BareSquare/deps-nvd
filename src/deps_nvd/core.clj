ac3c80d17d03d2e8d3330b93276b51b22ba33cb9
Stathis Sideris <stathis@pixelated-noise.com>
2019-04-10 18:12:26 +0300

    First commit

---
+++ src/deps_nvd/core.clj
@@ -0,0 +1,33 @@
+(ns deps-nvd.core
+  (:require [clojure.edn :as edn]
+            [clojure.data.json :as json]
+            [clojure.tools.deps.alpha :as deps]
+            [clojure.tools.deps.alpha.util.maven :as mvn]
+            [nvd.core]))
+
+(def temp-file (java.io.File/createTempFile ".deps-nvd_" ".json"))
+
+(defn get-jars [filename]
+  (->> (-> filename
+           slurp
+           edn/read-string
+           (update :mvn/repos merge mvn/standard-repos)
+           (deps/resolve-deps {}))
+       vals (mapcat :paths) distinct))
+
+(defn -main [deps-file command & args]
+  (let [config       (-> deps-file slurp edn/read-string :nvd)
+        path         (.getAbsolutePath temp-file)
+        project-name "fix"
+        opts         (merge
+                      (select-keys config [:group :version :nvd])
+                      {:name      project-name
+                       :classpath (get-jars deps-file)
+                       :cmd-args  args})]
+
+    (spit path (json/write-str opts))
+
+    (case command
+      "check"  (nvd.task.check/-main path)
+      "purge"  (nvd.task.purge-database/-main path)
+      "update" (nvd.task.update-database/-main path))))
